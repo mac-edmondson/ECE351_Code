630f690d0eb1974fccb4b1d8ccd2da3ae7050ae0
mac-edmondson <macdaddy288@gmail.com>
2022-02-08 05:15:19 -0800

    Lab3 finished

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -12,7 +12,7 @@ Created on Tue Feb 01 18:30:00 2022
 #                                                              #
 # ECE351-53                                                    #
 #                                                              #
-# Lab #2                                                       #
+# Lab #3                                                       #
 #                                                              #
 # 02/08/2022                                                   #
 #                                                              #
@@ -81,25 +81,6 @@ plt.show()
 #PART 2
 
 #1
-
-def convolve(f1, f2) : #f1 and f2 must be of the same length and have had the same input
-#Output will have same length as f1 and f2
-    if(len(f1) != len(f2)) :
-        print("Error: Functions are not of the same length in convolution function!")
-        return
-    else :
-        out = np.zeros(len(f1))     # Output value for return
-        mit = range(len(f1))               # Maximum iteration value
-        #dt = len(f1)
-        for i in mit :
-            sum = 0
-            if(f1[i] != 0) :
-                for n in range(i):
-                    sum += (f1[n] * f2[i-n]) #Summation for single point to get magnitude of convolution at i
-                out[i] = sum
-            else:
-                out[i] = 0
-        return out
     
 def my_conv(f1, f2):
     Nf1 = len(f1)
@@ -116,34 +97,62 @@ def my_conv(f1, f2):
                     except:
                         print(i, j)
     return result
+
+# def convolve(f1, f2) : #f1 and f2 must be of the same length and have had the same input
+# #Output will have same length as f1 and f2
+#     if(len(f1) != len(f2)) :
+#         print("Error: Functions are not of the same length in convolution function!")
+#         return
+#     else :
+#         out = np.zeros(len(f1))     # Output value for return
+#         mit = range(len(f1))               # Maximum iteration value
+#         #dt = len(f1)
+#         for i in mit :
+#             sum = 0
+#             if(f1[i] != 0) :
+#                 for n in range(i):
+#                     sum += (f1[n] * f2[i-n]) #Summation for single point to get magnitude of convolution at i
+#                 out[i] = sum
+#             else:
+#                 out[i] = 0
+#         return out
                     
-bound = 10
+bound = 20
 t = np.arange(0, bound + step_size, step_size)
-y1 = ss.u(t) - ss.u(t-3)
-y2 = ss.u(t-2) - ss.u(t-4)
-# y2 = np.zeros(len(t))
-# y2[round(1/step_size)] = 1
-# print(len(t))
-# print(round(12/step_size)+1)
-# print(t[round(2/step_size)])
-#print(t[])
-ysp = spsig.convolve(y1, y2, mode='same')
-ymf = my_conv(y1, y2)
+f1 = f1(t)
+f2 = f2(t)
+f3 = f3(t)
+# f1f2p = spsig.convolve(f1, f2, mode='full')
+# f2f3p = spsig.convolve(f2, f3, mode='full')
+# f1f3p = spsig.convolve(f1, f3, mode='full')
+f1f2m = my_conv(f1, f2)
+print("1 done")
+f2f3m = my_conv(f2, f3)
+print("2 done")
+f1f3m = my_conv(f1, f3)
+print("3 done")
 t = np.arange(0, bound*2 + step_size, step_size)
-y1 = ss.u(t) - ss.u(t-3)
-y2 = ss.u(t-2) - ss.u(t-4) 
 #print(y[0])       
 
 plt.figure(figsize = (10, 11))
-plt.subplot(1, 1, 1)
-# plt.plot(t, ysp, "r-")
-plt.plot(t, ymf, "r--")
-plt.plot(t, y1, "g--")
-plt.plot(t, y2, "b--")
-#plt.ylim(-1, 5)
-plt.xlim(0,10)
+plt.subplot(3, 1, 1)
+# plt.plot(t, f1f2p, "b-")
+plt.plot(t, f1f2m, "b-")
 plt.grid()
-plt.ylabel('f_1(t)')
-plt.title('f_1(t), f_2(t), f_3(t) v. t')
+plt.ylabel('f_1 * f_2')
+plt.title('Convolutions of f1, f2, f3')
+
+plt.subplot(3, 1, 2)
+# plt.plot(t, f2f3p, "g-")
+plt.plot(t, f2f3m, "g-")
+plt.grid()
+plt.ylabel('f_2 * f_3')
+
+plt.subplot(3, 1, 3)
+# plt.plot(t, f1f3p, "r-")
+plt.plot(t, f1f3m, "r-")
+plt.grid()
+plt.ylabel('f_2 * f_3')
+
 plt.show()
         
